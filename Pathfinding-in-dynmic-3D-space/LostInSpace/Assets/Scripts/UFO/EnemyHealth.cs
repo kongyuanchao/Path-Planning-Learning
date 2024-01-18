b1c22241ef244cbf290c4b34790da61051959b44
kongyuanchao@hotmail.com <kongyuanchao@hotmail.com>
2024-01-18 21:10:50 +0800

    Upload source code from CarinaKr

---
+++ Pathfinding-in-dynmic-3D-space/LostInSpace/Assets/Scripts/UFO/EnemyHealth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : UFOHealth {
+    
+
+    private UFOController controller;
+
+    // Use this for initialization
+    protected override void Start () {
+        base.Start();
+        controller = GetComponent<UFOController>();
+	}
+    
+    
+
+    public override void GameOver()
+    {
+        Debug.Log("fail");
+        controller.Reset();
+        Refill();
+    }
+}
