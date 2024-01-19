daab3f355a8f7f4e3c271d75c3e247bd58c84db3
kongyuanchao@hotmail.com <kongyuanchao@hotmail.com>
2024-01-19 16:53:18 +0800

    Update today's contents 19/01/2024

--- Pathfinding-in-dynmic-3D-space/LostInSpace/Assets/Scripts/Pathfinding/SearchGraph/UITest.cs
+++ Pathfinding-in-dynmic-3D-space/LostInSpace/Assets/Scripts/Pathfinding/SearchGraph/UITest.cs
@@ -18,6 +18,14 @@ public class UITest : MonoBehaviour {
     public Vector3 startPosition;
     public Vector3 goalPosition;
 
+    
+    [Range(float.NegativeInfinity, float.PositiveInfinity)] public float x;
+    [Range(float.NegativeInfinity, float.PositiveInfinity)] public float y;
+    [Range(float.NegativeInfinity, float.PositiveInfinity)] public float z;
+    [Range(-360, 360)] public float rx;
+    [Range(-360, 360)] public float ry;
+    [Range(-360, 360)] public float rz;
+
     public bool isUpdatingGraph { get; private set; }
 
     private float _intervalTime;
@@ -29,7 +37,23 @@ public class UITest : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
     //Debug.Log($"Voxel size:  {2} and Transform: {transform.position.ToString()}");
-    startPosition = transform.position;
-    goalPosition = transform.rotation.eulerAngles;
+        x = transform.position.x;
+        y = transform.position.y;
+        z = transform.position.z;
+        rx = transform.rotation.eulerAngles.x;
+        ry = transform.rotation.eulerAngles.y;
+        rz = transform.rotation.eulerAngles.z;
+
+        Collider[] colliders = Physics.OverlapBox(transform.position, new Vector3(0.5f,0.5f,0.5f), Quaternion.identity);
+
+        //if (colliders.Length > 0)
+        //{
+        //    Debug.Log("Inside collider1");
+        //}
+
+        foreach (var item in colliders)
+        {
+            Debug.Log($"Collide Object:  {item.gameObject.name}" );
+        }
     }
 }
