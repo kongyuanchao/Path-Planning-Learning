2e2585580ebfa63b0920ada74ad23f7fd155ad22
kongyuanchao@hotmail.com <kongyuanchao@hotmail.com>
2024-01-20 16:54:32 +0800

    Update some stuff 20/1/2024

--- Pathfinding-in-dynmic-3D-space/LostInSpace/Assets/Scripts/Pathfinding/SearchGraph/UITest.cs
+++ Pathfinding-in-dynmic-3D-space/LostInSpace/Assets/Scripts/Pathfinding/SearchGraph/UITest.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class UITest : MonoBehaviour {
@@ -8,7 +9,11 @@ public class UITest : MonoBehaviour {
         CELL_GRID,
         WAYPOINTS
     }
-
+    public int searchWidth, searchHeight, searchLength;
+    public float cellSize;
+    public GameObject cellPrefab;
+    public List<Node> nodes { get; set; }
+    private Cell[,,] cellArray;
     public CreatingMethod creationMethod;
     public Transform start, goal;
 
@@ -31,20 +36,20 @@ public class UITest : MonoBehaviour {
     private float _intervalTime;
     // Use this for initialization
     void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-    //Debug.Log($"Voxel size:  {2} and Transform: {transform.position.ToString()}");
-        x = transform.position.x;
-        y = transform.position.y;
-        z = transform.position.z;
-        rx = transform.rotation.eulerAngles.x;
-        ry = transform.rotation.eulerAngles.y;
-        rz = transform.rotation.eulerAngles.z;
+        StartCoroutine("CreateCellGrid");
+    }
 
-        Collider[] colliders = Physics.OverlapBox(transform.position, new Vector3(0.5f,0.5f,0.5f), Quaternion.identity);
+    // Update is called once per frame
+    void Update () {
+        //Debug.Log($"Voxel size:  {2} and Transform: {transform.position.ToString()}");
+        //x = transform.position.x;
+        //y = transform.position.y;
+        //z = transform.position.z;
+        //rx = transform.rotation.eulerAngles.x;
+        //ry = transform.rotation.eulerAngles.y;
+        //rz = transform.rotation.eulerAngles.z;
+
+        //Collider[] colliders = Physics.OverlapBox(transform.position, new Vector3(0.5f,0.5f,0.5f), Quaternion.identity);
 
         //if (colliders.Length > 0)
         //{
@@ -55,5 +60,44 @@ public class UITest : MonoBehaviour {
         //{
         //    Debug.Log($"Collide Object:  {item.gameObject.name}");
         //}
+
     }
+    private void SetNodes()
+    {
+        nodes = new List<Node>();
+        for (int z = 0; z < searchWidth; z++)
+        {
+            for (int y = 0; y < searchHeight; y++)
+            {
+                for (int x = 0; x < searchLength; x++)
+                {
+
+                    nodes.Add(cellArray[x, y, z]);
+                }
+            }
+        }
+    }
+    public IEnumerator CreateCellGrid()
+    {
+        cellArray = new Cell[searchLength, searchHeight, searchWidth];
+        for (int z = 0; z < searchWidth; z++)
+        {
+            for (int y = 0; y < searchHeight; y++)
+            {
+                for (int x = 0; x < searchLength; x++)
+                {
+                    cellArray[x, y, z] = CellObject.Create(cellPrefab, transform,
+                        new Vector3(x, y, z) * cellSize, cellSize);
+                    //UnityEngine.Debug.Log($"Voxel size:  {cellSize} and Transform: {transform.position.ToString()}");
+                }
+            }
+        }
+        yield return null;
+        UnityEngine.Debug.Log($"Width: {searchWidth}, Height: {searchHeight}, Length: {searchLength} = " +
+            $"{searchWidth * searchHeight * searchLength}");
+        SetNodes();
+        //base.GraphBuild();
+        StopCoroutine("CreateCellGrid");
+    }
+
 }
